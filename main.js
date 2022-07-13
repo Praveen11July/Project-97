f3d527e640df26f215a6a19df7ce2cb0154adfb7
Praveen11July <106472504+Praveen11July@users.noreply.github.com>
2022-07-13 17:33:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+function addition()  {
+    var z = 
+        Number(document.getElementById("firstno").value)  
+    + 
+    Number(document.getElementById("secondno").value) ;
+     document.getElementById("addition").innerHTML ="Your Answer is:"+ z;
+    
+   }
+   function subtract()  {
+    var y = 
+        Number(document.getElementById("firstnom").value)  
+    -
+    Number(document.getElementById("secondnom").value) ;
+    document.getElementById("subtract").innerHTML ="Your Answer is:"+ y;
+    
+     
+    
+   }
+   function multiply()  {
+    var x = 
+        Number(document.getElementById("first").value)  
+    *
+    Number(document.getElementById("second").value) ;
+    document.getElementById("multiply").innerHTML ="Your Answer is:"+ x;
+    
+     
+     
+     }
+     function division()  {
+        var a = 
+            Number(document.getElementById("first").value)  
+        /
+        Number(document.getElementById("second").value) ;
+        document.getElementById("division").innerHTML ="Your Answer is:"+ a;
+        
+         
+         
+         }
+
+
+
+
+
+    
+
+
+
+     
+   
+   
